53aff879e1d5067b9e74719319aa7e39ca37916d
Rarynn <94757950+Rarynn@users.noreply.github.com>
2023-03-15 09:23:24 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# WE4A Project
+ 
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="shortcut icon" href="" type="image/x-icon"></link>
+        <link rel="stylesheet" href="">
+        <title>MyNetWork</title>
+    </head>
+
+
+
+    <body>
+        
+    </body>
+</html>
