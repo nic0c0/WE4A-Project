2c39e16eee483dfbf692379e728bb6ad3987deb2
Rarynn <94757950+Rarynn@users.noreply.github.com>
2023-03-28 18:02:57 +0200

    creation variables
    creation var + correction post

--- Parties/post.php
+++ Parties/post.php
@@ -1,5 +1,6 @@
 <div class="post">
-    <div class="left"> <p>test</p></div>
+    <div class="left"> <h1>Le bibimbapp </h1>
+    <img src="./IMG/bibimbap.jpg" alt="bibimbap"></div>
     <div class="center"> <p>1_ heritage simple : but : généricité et factorisation du code
     -public -> modelise la notion de sous classe
     -protected -> modelise la notion de classe amie
