b11f5d5f3c877f3d131a54eeb1469076b470a6e6
Rarynn <94757950+Rarynn@users.noreply.github.com>
2023-04-18 11:15:34 +0200

    settings profil

--- redirect.php
+++ redirect.php
@@ -33,8 +33,35 @@ switch ($path) {
             $post_img=saveImageAsNew($user_id,false,$postid);
             //Mise à jour de la base de données
             $conn->insertPost($user_id, $post_title, $post_text, $post_img);
-            header("Location: ./index.php");
         }
+        header("Location: ./index.php");
+        exit();
+    case 'Settings.php':
+        //on traite les infos pour les settings
+        //si c'est un changement dans le profil
+        if (isset($_POST['save'])) {
+            $user_email = $_POST['user_email'];
+            $user_name = $_POST['user_name'];
+            $user_surname = $_POST['user_surname'];
+            $user_desc= $_POST['user_desc'];
+            $user_id = $_POST['user_id'];
+            $path = $_POST['path'];
+    
+            // Chargement de l'image de profil
+            $user_pp = $_POST['user_pp'];
+            if (isset($_FILES['new_pp']) && $_FILES['new_pp']['size'] != 0) {
+                $new_pp = saveImageAsNew($user_id,true,0);
+                if ($new_pp) {
+                    $user_pp = $new_pp;
+                }
+            }
+    
+    
+            // Mise à jour des données utilisateur dans la base de données
+            $conn->updateProfile($user_id, $user_email, $user_pp,$user_name, $user_surname,$user_desc);    
+
+        }
+        header("Location: $path");
         exit();
     default:
         header("Location: ./Settings.php?path=$path");
