2e69b0426b602286f0ef60ae37f3d878f4c7b280
nic0c0 <99414348+nic0c0@users.noreply.github.com>
2023-04-18 11:28:17 +0200

    Revert "commit"
    This reverts commit e83798835fc6592397f29e07833e35e9a849f99e.

--- Settings.php
+++ Settings.php
@@ -6,13 +6,8 @@
 
 $cook = new Cookie();
 
-if($cook->CheckIntegrity()){
-    echo "problem d'intégrité de vos informations!";
-}
-
 // Vérification si l'utilisateur est authentifié
 if ($cook->IssetCookie()) {
-    echo "INTEGRITE!";
     $authenticated = true;
 
     // Connexion à la base de données
@@ -22,59 +17,26 @@ if ($cook->IssetCookie()) {
     $user_data = $conn->getUserData($cook->getUsername());
     $user_id = $user_data['user_id'];
 
-    // Traitement des données soumises par le formulaire
-    if (isset($_POST['save'])) {
-        $user_email = $_POST['user_email'];
-        $user_name = $_POST['user_name'];
-        $user_surname = $_POST['user_surname'];
-        $user_desc= $_POST['user_desc'];
-
-    // Chargement de l'image de profil
-    $user_pp = $user_data['user_pp'];
-    if (isset($_FILES['new_pp']) && $_FILES['new_pp']['size'] != 0) {
-        $new_pp = saveImageAsNew($user_id,true,0);
-        if ($new_pp) {
-            $user_pp = $new_pp;
-        }
-    }
-
-
-        // Mise à jour des données utilisateur dans la base de données
-        $conn->updateProfile($user_id, $user_email, $user_pp,$user_name, $user_surname,$user_desc);
-
-        // header("Location: ./Settings.php");
-        // exit();
-    }
 
     if (isset($_POST['ChangePassword'])){
  
-        $OLDPASSWORD=htmlentities($_POST["oldpassword"]);
         $PASSWORD1=htmlentities($_POST["password1"]);
         $PASSWORD2=htmlentities($_POST["password2"]);
-
-        $conn = new SQLconn();
-        if($conn->CheckDB($cook->getUsername(), $OLDPASSWORD)){
-
+    
+        if($PASSWORD1==$PASSWORD2){
+    
             
-            if($PASSWORD1==$PASSWORD2){
-        
-                
+            $conn = new SQLconn();
+            $hash= EncryptedPaswword($PASSWORD1);
+            $conn->updatePassword($cook->getUsername(),$hash);
+            $cook->UpdatePassword($hash);
             
-                $hash= EncryptedPaswword($PASSWORD1);
-                $conn->updatePassword($cook->getUsername(),$hash);
-                $cook->UpdatePassword($hash);
-                
-                
-            }else{
-                
-                echo "mauvais mots de passe";
-        
-            }
-
-            }else{
-                echo "Ancien mot de passe incorrect!";
-            }
-
+            
+        }else{
+            
+            echo "mauvais mots de passe";
+    
+        }
     
     }
 
@@ -92,7 +54,7 @@ if ($cook->IssetCookie()) {
 <div class="center">
   <div class="settings">
   <h1>Paramètres</h1>
-    <form method="post" class="set1" enctype="multipart/form-data" >
+    <form method="post" class="set1" enctype="multipart/form-data" action="redirect.php" >
       <label for="user_email">Adresse email :</label>
       <input type="email" id="user_email" name="user_email" value="<?php echo !empty($user_data['user_email']) ? $user_data['user_email'] : ''; ?>" placeholder="<?php echo empty($user_data['user_email']) ? 'Non renseigné' : ''; ?>" required>
       
@@ -119,6 +81,11 @@ if ($cook->IssetCookie()) {
             </div>
             <button class="btn button full" type="submit" name="save">Sauvegarder</button>
         </fieldset>
+        // On envoie le path pour pouvoir revenir à la page d'origine
+        <input type="hidden" name="path" value="<?php echo basename(__FILE__); ?>">
+        <input type="hidden" name="user_id" value="<?php echo $user_id; ?>">
+        <input type="hidden" name="user_pp" value="<?php echo $user_data['user_pp']; ?>">
+ 
     
     </form>
 
@@ -126,13 +93,10 @@ if ($cook->IssetCookie()) {
     <form method="post" enctype="multipart/form-data" >
             <fieldset>
                 <legend>Changer de Mot de passe</legend>
-                <label for="oldpassword">Ancien mot de passe :
-                    <input id="oldpassword" type="password" placeholder="Password" name="oldpassword" required>
-                </label>
-                <label for="password1">Nouveau Mot de passe :
+                <label for="password1">Mot de passe :
                     <input id="password1" type="password" placeholder="Password" name="password2" required>
                 </label>
-                <label for="password2">Nouveau Mot de passe :
+                <label for="password2">Mot de passe :
                     <input id="password2" type="password" placeholder="Password" name="password1" required>
                 </label>
             </fieldset>
