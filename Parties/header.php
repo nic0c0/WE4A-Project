1ad12d7c1c460bdda902019d717c6c40a65e9aed
nic0c0 <99414348+nic0c0@users.noreply.github.com>
2023-04-18 18:44:08 +0200

    bar de recherche dynamique

--- Parties/header.php
+++ Parties/header.php
@@ -5,7 +5,43 @@
         </a>
     </div>
     <form class="search">
-        <input type="text" placeholder="Rechercher...">
+    <script>
+
+//Variable globale
+previousText = "";
+timer = 0;
+
+//Timer qui boucle toutes les secondes pour changer la variable globale
+function TimerIncrease() {
+  timer+=1000;
+  setTimeout('TimerIncrease()',1000);
+}
+TimerIncrease();
+
+function suggestNamesFromInput(currentText) {
+
+  if (currentText != previousText && timer >= 1000 ){
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+      document.getElementById("suggestions").innerHTML = this.responseText;
+      }
+    xhttp.open("GET", "./loadMe2.php?var=" + currentText , true); //Le booléen final dit si le chargement est asynchrone ou non
+    xhttp.send();
+
+    previousText = currentText;
+    timer = 0;
+  }
+  
+}
+
+function autoFillName(nametext){
+  document.getElementById("suggestField").value = nametext;
+}
+
+</script>
+  <input id="suggestField" type="text" onkeyup="suggestNamesFromInput(this.value)">
+  <p id="suggestions"><i></i></p>
+</div>
         <button type="submit">Go</button>
     </form>
     <nav>
